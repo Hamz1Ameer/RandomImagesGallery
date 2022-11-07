d07ec21656a5f7799fc4a604a923eefe2d21d316
Hamz1Ameer <101882275+Hamz1Ameer@users.noreply.github.com>
2022-11-08 00:25:21 +0530

    Update DateTime.js
    Addded a checkTime() function to check time and return Morning/Noon/Evening as a string, which is passed to Greetings Component with the help of props.
    
    created date and time variables to store the values of current date and time. which is then passed to the JSX.

--- Components/DateTime.js
+++ Components/DateTime.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Greetings from "./Greetings";
 const DateTime=()=>{
     let currentDateTime = new Date();
+    let date=currentDateTime.toLocaleDateString();
+    let time=currentDateTime.toLocaleTimeString();
+    
+    const checkTime=()=>{
+        if((currentDateTime.getHours()>=0) && (currentDateTime.getHours()<12)){
+            return 'Good Morning'
+        }
+        else if((currentDateTime.getHours()>=12) && (currentDateTime.getHours()<20)){
+            return 'Good After Noon';
+        }
+        else{
+            return 'Good Evening';
+        }
+    }
+    let greet=checkTime();
     return  <>
-        <h3 contentEditable='true'>Hi there, my name is Mohammed Ameer Hamza</h3>
-        
-        <div>Todays date is: {currentDateTime.toLocaleDateString()} </div>
-        <div>Current Time: {currentDateTime.toLocaleTimeString()}</div>
+        <Greetings greet={greet}/>
+       <div>Today's date is: {date} </div>
+        <div>Current Time: {time}</div>
     </>
 }
 export default DateTime;
